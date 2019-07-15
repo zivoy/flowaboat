6979f0baf8456dd7a88f2ee8b4f1bebda05aa092
zivoy <ziv132@gmail.com>
2019-07-14 23:38:37 -0400

    testing for functions

--- tail.js
+++ tail.js
@@ -1,11 +1,12 @@
-var sys = require('sys');
-var filename =  process.argv[0];
+const sys = require('sys');
+const filename =  process.argv[0];
+const childs = require('child_process');
 
 if (!filename)
   return sys.puts("Usage: node watcher.js filename");
 
 // Look at http://nodejs.org/api.html#_child_processes for detail.
-var tail = process.createChildProcess("tail", ["-f", filename]);
+var tail = childs.spawn("tail", ["-f", filename]);
 sys.puts("start tailing");
 
 tail.addListener("output", function (data) {
