113254ab3a5af0cdec0928ff5a7672e0957294f9
zivoy <ziv132@gmail.com>
2019-07-15 00:34:17 -0400

    listeners

--- tail.js
+++ tail.js
@@ -9,15 +9,16 @@ if (!filename)
 var tail = childs.spawn("tail", ["-f", filename]);
 console.log("start tailing");
 
-tail.addListener("output", function (data) {
-	console.log(data);
+tail.stdout.on('data', function(data) {
+	console.log(data.toString());
 });
 
 // From nodejs.org/jsconf.pdf slide 56
 http.createServer(function (req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	tail.addListener("output", function (data) {
+	tail.stdout.on('data', function(data) {
 		res.write(data);
 	});
+
 	res.end();
 }).listen(8000);
