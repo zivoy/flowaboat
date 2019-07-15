866f101bbdb9f48adb6be51cd9bff7b8abcf8223
zivoy <ziv132@gmail.com>
2019-07-15 19:06:45 -0400

    flls dum

--- tail.js
+++ tail.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(http);
 if (!filename)
 	return console.log("Usage: node tail.js log folder");
 
-var tail = childs.spawn("tail", ["-f", filename]);
+var tail = childs.spawn("tail", ["-f", filename + '/log.log'];
 console.log("start tailing");
 
 tail.stdout.setEncoding('utf8');
@@ -21,11 +21,6 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/logRender.html');
 });
 
-/*
-app.get('/', function(req, res){
-	res.redirect("/log");
-});
-
 app.get('/log', function(req, res){
 	res.sendFile(filename + '/log.log', {root: __dirname});
 });
@@ -36,25 +31,15 @@ app.get('/out', function(req, res){
 
 app.get('/err', function(req, res){
 	res.sendFile(filename + '/err.log', {root: __dirname});
-});*/
+});
 
 
-io.on('connection', function(){
+io.on('connection', function(socket){
 	tail.stdout.on('data', function(data) {
 		io.emit('log output', data.toString());
 	});
 });
 
-http.listen(80, function(){
+http.listen(80, function(socket){
 	console.log('listening on *:80');
 });
-
-/*
-// From nodejs.org/jsconf.pdf slide 56
-http.createServer(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	tail.stdout.on('data', function(data) {
-		res.write(data.toString(), function(err) { netSocket.end(); });
-	});
-}).listen(8000);
- */
