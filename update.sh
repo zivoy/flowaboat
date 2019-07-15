b1b542a4d614eda4a6f35d853b97a4fdffa36cb1
zivoy <ziv132@gmail.com>
2019-07-15 18:23:13 -0400

    update packages

--- update.sh
+++ update.sh
@@ -3,3 +3,9 @@ git checkout master
 git pull origin master
 git pull incoming master
 echo "synced"
+
+git add package-lock.json
+git add package.json
+git commit -m "cleaning packages"
+git push
+echo "Packages sent"
