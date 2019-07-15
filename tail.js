5ea6db9123729c4676bc6f59918362f702e3da1b
zivoy <ziv132@gmail.com>
2019-07-15 01:28:06 -0400

    changing method

--- tail.js
+++ tail.js
@@ -1,10 +1,11 @@
 const filename = process.argv[2];
-const childs = require('child_process');
-const http = require("http");
+//const childs = require('child_process');
+const app = require('express')();
+const http = require('http').createServer(app);
 
 if (!filename)
 	return console.log("Usage: node watcher.js filename");
-
+/*
 // Look at http://nodejs.org/api.html#_child_processes for detail.
 var tail = childs.spawn("tail", ["-f", filename]);
 console.log("start tailing");
@@ -13,12 +14,22 @@ tail.stdout.setEncoding('utf8');
 tail.stdout.on('data', function(data) {
 	console.log(data.toString());
 });
+*/
 
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/logs/log.txt');
+});
+
+http.listen(8000, function(){
+	console.log('listening on *:8000');
+});
+
+/*
 // From nodejs.org/jsconf.pdf slide 56
 http.createServer(function (req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	tail.stdout.on('data', function(data) {
-		res.write(data.toString());
+		res.write(data.toString(), function(err) { netSocket.end(); });
 	});
-//	res.end();
 }).listen(8000);
+ */
