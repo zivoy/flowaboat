579ad0600c8844f8e22fe7c4a0e5b8586180afdd
zivoy <ziv132@gmail.com>
2019-07-15 21:09:28 -0400

    this order will be better

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-git checkout master
-git pull origin master
-git pull incoming master
-echo "synced"
-
 git add package-lock.json
 git add package.json
 git commit -m "cleaning packages"
-git push
 echo "Packages sent"
+
+git checkout master
+git pull origin master
+git pull incoming master
+
+git push
+echo "synced"
