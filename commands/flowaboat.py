e957c3fa5d7f0e58e6250c8943e666f415552851
zivoy <ziv132@gmail.com>
2019-08-12 03:06:33 -0400

    bot info

---
+++ commands/flowaboat.py
@@ -0,0 +1,29 @@
+from utils import *
+from discord import Embed
+
+
+class Command:
+    command = "flowaboat"
+    description = "Show information about this bot."
+    argsRequired = 0
+    usage = ""
+    example = {
+        "run": "flowaboat",
+        "result": "Tells you about flowaboat."
+    }
+    synonyms = ["info"]
+
+    async def call(self, package):
+        message = package["message_obj"]
+
+        embed = Embed(description="Modular Discord bot with advanced osu! commands.",
+                      url="https://github.com/zivoy/flowaboat", color=0xcf660a)
+        embed.set_footer(icon_url="https://avatars1.githubusercontent.com/u/16857861?s=64&v=2", text="zivoy")
+        embed.set_thumbnail(
+            url="https://yt3.ggpht.com/a/AGF-l78A7VJcEml4nmoU7xP7bXRbnukYxDy0t1UiKQ=s288-c-k-c0xffffffff-no-rj-mo")
+        embed.set_author(name="flowaboat", url="https://github.com/zivoy/flowaboat")
+        embed.add_field(name="GitHub Repo", value="https://github.com/zivoy/flowaboat")
+        embed.add_field(name="Commands", value="Not all commands supported yet \n \
+                                                https://github.com/zivoy/flowaboat/blob/master/COMMANDS.md")
+        embed.add_field(name="Prefix", value=f"The command prefix on this bot is `{Config.prefix}`.")
+        await message.channel.send(embed=embed)
