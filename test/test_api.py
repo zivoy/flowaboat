9efdd2b7a6ff382f77f0d717307d437dd6502e23
zivoy <ziv132@gmail.com>
2019-08-21 20:20:11 -0400

    unit test test

---
+++ test/test_api.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest import mock
+import arrow
+import utils
+
+date_form = "YYYY-MM-DD hh:mm:ss"
+sample_date = arrow.get(2019, 1, 1, 13, 14, 15)
+
+
+class TestAPIMethods(unittest.TestCase):
+
+    @mock.patch('arrow.utcnow', mock.MagicMock(return_value=sample_date))
+    def test_clear_queue(self):
+        api = utils.Api('http://foo.com', 4)
+        time4 = sample_date
+        api.actions = []
+
+        api.actions.append(time4.shift(seconds=-60))
+        self.assertEqual(len(api.actions), 1)
+
+        api.clear_queue()
+        self.assertEqual(len(api.actions), 0)
+        api.actions.append(time4.shift(seconds=-60))
+        api.actions.append(time4)
+        api.actions.append(time4)
+        self.assertEquals(len(api.actions), 3)
+        api.clear_queue()
+        self.assertEquals(len(api.actions), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
